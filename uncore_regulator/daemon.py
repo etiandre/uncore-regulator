d4e7d310787711d847a990fea1e4a50373433af1
Étienne ANDRÉ <eti.andre@gmail.com>
2019-07-15 14:43:57 +0200

    multiple core & sockets support

--- uncore_regulator/daemon.py
+++ uncore_regulator/daemon.py
@@ -21,22 +21,26 @@ class Daemon():
     def set_logfile(self, logfile):
         self._logfile = open(logfile, 'w')
 
-    def __init__(self, cores, logfile=None, regulator=None):
+    def __init__(self, cores, sockets, logfile=None, regulator=None):
         if logfile:
             logging.info("Logging meters to {}".format(logfile))
             self._logfile = open(logfile, 'w')
         else:
+            logging.info("Not logging anything")
             self._logfile = None
         if regulator:
             logging.info("Using regulator {}".format(regulator))
-            r_module = importlib.import_module(
-                "uncore_regulator.regulators." + regulator) 
+            r_module = importlib.import_module("uncore_regulator.regulators." +
+                                               regulator)
             self._regulator = getattr(r_module, regulator)()
         else:
+            logging.info("No regulator loaded")
             self._regulator = None
-        logging.info("Reading cores {}".format(cores))
+        logging.info("Using cores {} and sockets {}".format(cores, sockets))
         self.cores = cores
-
+        self.sockets = sockets
+        self.measured_cores = list(set.union(cores, sockets))
+        logging.info("-> Metering on {}".format(self.measured_cores))
         # https://github.com/RRZE-HPC/likwid/blob/master/groups/broadwellEP/MEM_DP.txt
         estr = "FP_ARITH_INST_RETIRED_128B_PACKED_DOUBLE:PMC0,"
         estr += "FP_ARITH_INST_RETIRED_SCALAR_DOUBLE:PMC1,"
@@ -60,17 +64,18 @@ class Daemon():
         estr += "PWR_PKG_ENERGY:PWR0,"
         estr += "PWR_DRAM_ENERGY:PWR3,"
         estr += "UNCORE_CLOCK:UBOXFIX"
-        pylikwid.init(self.cores)
-        print("initialized {} cores".format(pylikwid.getnumberofthreads()))
+        pylikwid.init(self.measured_cores)
+        logging.info("initialized {} threads".format(pylikwid.getnumberofthreads()))
         self._gid = pylikwid.addeventset(estr)
-        logging.info("added event set " + estr)
         pylikwid.setup(self._gid)
         signal.signal(signal.SIGINT, self._exit_handler)
 
     def run(self):
         self._logtofile(
-            "t [s]\tMFLOP/s\tMEM bandwidth [MB/s]\tOperational Intensity\tPower 0 [W]\tPower 1 [W]\tDRAM Pow. 0 [W]\tDRAM Pow. 1 [W]\tUncore freq. 0 [MHz]\tUncore freq. 1 [Mhz]"
-        )
+            "t [s]\tMFLOP/s\tMEM bandwidth [MB/s]\tOperational Intensity\t" +
+            "\t".join(
+                "Power {0} [W]\tDRAM Pow. {0} [W]\tUncore freq. {0} [MHz]".
+                 format(i) for i in self.sockets))
         for i in range(0, pylikwid.getnumberofevents(self._gid)):
             logging.info("event {}: {}".format(
                 i, pylikwid.getnameofevent(self._gid, i)))
@@ -88,64 +93,81 @@ class Daemon():
             t = tprime
             d["t"] = t
             d["dt"] = dt
-            # Read PMCs on all self.cores of socket #0
+            # Read PMCs on all cores
             flop = 0
-            for core in range(0, 28, 2):
+            for core in self.cores:
                 PMC0 = pylikwid.getlastresult(self._gid, 0,
-                                              self.cores.index(core))
+                                              self.measured_cores.index(core))
                 PMC1 = pylikwid.getlastresult(self._gid, 1,
-                                              self.cores.index(core))
+                                              self.measured_cores.index(core))
                 PMC2 = pylikwid.getlastresult(self._gid, 2,
-                                              self.cores.index(core))
+                                              self.measured_cores.index(core))
                 flop += (PMC0 * 2.0 + PMC1 + PMC2 * 4.0)
             d["flop/s"] = flop / dt
-            # Read MBOXes only on socket #0 (socket 1 bug :( )
-            MBOX0C0 = pylikwid.getlastresult(self._gid, 3, 0)
-            MBOX0C1 = pylikwid.getlastresult(self._gid, 4, 0)
-            MBOX1C0 = pylikwid.getlastresult(self._gid, 5, 0)
-            MBOX1C1 = pylikwid.getlastresult(self._gid, 6, 0)
-            MBOX2C0 = pylikwid.getlastresult(self._gid, 7, 0)
-            MBOX2C1 = pylikwid.getlastresult(self._gid, 8, 0)
-            MBOX3C0 = pylikwid.getlastresult(self._gid, 9, 0)
-            MBOX3C1 = pylikwid.getlastresult(self._gid, 10, 0)
-            MBOX4C0 = pylikwid.getlastresult(self._gid, 11, 0)
-            MBOX4C1 = pylikwid.getlastresult(self._gid, 12, 0)
-            MBOX5C0 = pylikwid.getlastresult(self._gid, 13, 0)
-            MBOX5C1 = pylikwid.getlastresult(self._gid, 14, 0)
-            MBOX6C0 = pylikwid.getlastresult(self._gid, 15, 0)
-            MBOX6C1 = pylikwid.getlastresult(self._gid, 16, 0)
-            MBOX7C0 = pylikwid.getlastresult(self._gid, 17, 0)
-            MBOX7C1 = pylikwid.getlastresult(self._gid, 18, 0)
-            # Read memory consumption on socket #0
-            datavolume = (
-                (MBOX0C0 + MBOX1C0 + MBOX2C0 + MBOX3C0 + MBOX4C0 + MBOX5C0 +
-                 MBOX6C0 + MBOX7C0 + MBOX0C1 + MBOX1C1 + MBOX2C1 + MBOX3C1 +
-                 MBOX4C1 + MBOX5C1 + MBOX6C1 + MBOX7C1) * 64.0)
-            d["memory-bandwidth"] = datavolume / dt
-            # read PWR and uncore freq on 0,1
-            PWR0_0 = pylikwid.getlastresult(self._gid, 19, 0)
-            PWR0_1 = pylikwid.getlastresult(self._gid, 19, 1)
-            PWR3_0 = pylikwid.getlastresult(self._gid, 20, 0)
-            PWR3_1 = pylikwid.getlastresult(self._gid, 20, 1)
-            UNCORE_CLOCK_0 = pylikwid.getlastresult(self._gid, 21, 0)
-            UNCORE_CLOCK_1 = pylikwid.getlastresult(self._gid, 21, 1)
 
+            # Read sockets
+            datavolume = 0
+            for socket in self.sockets:
+                MBOX0C0 = pylikwid.getlastresult(
+                    self._gid, 3, self.measured_cores.index(socket))
+                MBOX0C1 = pylikwid.getlastresult(
+                    self._gid, 4, self.measured_cores.index(socket))
+                MBOX1C0 = pylikwid.getlastresult(
+                    self._gid, 5, self.measured_cores.index(socket))
+                MBOX1C1 = pylikwid.getlastresult(
+                    self._gid, 6, self.measured_cores.index(socket))
+                MBOX2C0 = pylikwid.getlastresult(
+                    self._gid, 7, self.measured_cores.index(socket))
+                MBOX2C1 = pylikwid.getlastresult(
+                    self._gid, 8, self.measured_cores.index(socket))
+                MBOX3C0 = pylikwid.getlastresult(
+                    self._gid, 9, self.measured_cores.index(socket))
+                MBOX3C1 = pylikwid.getlastresult(
+                    self._gid, 10, self.measured_cores.index(socket))
+                MBOX4C0 = pylikwid.getlastresult(
+                    self._gid, 11, self.measured_cores.index(socket))
+                MBOX4C1 = pylikwid.getlastresult(
+                    self._gid, 12, self.measured_cores.index(socket))
+                MBOX5C0 = pylikwid.getlastresult(
+                    self._gid, 13, self.measured_cores.index(socket))
+                MBOX5C1 = pylikwid.getlastresult(
+                    self._gid, 14, self.measured_cores.index(socket))
+                MBOX6C0 = pylikwid.getlastresult(
+                    self._gid, 15, self.measured_cores.index(socket))
+                MBOX6C1 = pylikwid.getlastresult(
+                    self._gid, 16, self.measured_cores.index(socket))
+                MBOX7C0 = pylikwid.getlastresult(
+                    self._gid, 17, self.measured_cores.index(socket))
+                MBOX7C1 = pylikwid.getlastresult(
+                    self._gid, 18, self.measured_cores.index(socket))
+                datavolume += ((MBOX0C0 + MBOX1C0 + MBOX2C0 + MBOX3C0 +
+                                MBOX4C0 + MBOX5C0 + MBOX6C0 + MBOX7C0 +
+                                MBOX0C1 + MBOX1C1 + MBOX2C1 + MBOX3C1 +
+                                MBOX4C1 + MBOX5C1 + MBOX6C1 + MBOX7C1) * 64.0)
+                # read PWR and uncore freq on sockets
+                PWR0 = pylikwid.getlastresult(
+                    self._gid, 19, self.measured_cores.index(socket))
+                d["pkg-power_" + str(socket)] = PWR0 / d["dt"]
+                PWR3 = pylikwid.getlastresult(
+                    self._gid, 20, self.measured_cores.index(socket))
+                d["dram-power_" + str(socket)] = PWR3 / d["dt"]
+                UNCORE_CLOCK = pylikwid.getlastresult(self._gid, 21, 0)
+                d["uncore-freq_" + str(socket)] = UNCORE_CLOCK / d["dt"]
+
+            d["memory-bandwidth"] = datavolume / dt
             if datavolume != 0: d["operational-intensity"] = flop / datavolume
             else: d["operational-intensity"] = float('inf')
-
-            d["pkg-power_0"] = PWR0_0 / d["dt"]
-            d["pkg-power_1"] = PWR0_1 / d["dt"]
-            d["dram-power_0"] = PWR3_0 / d["dt"]
-            d["dram-power_1"] = PWR3_1 / d["dt"]
-            d["uncore-freq_0"] = UNCORE_CLOCK_0 / d["dt"]
-            d["uncore-freq_1"] = UNCORE_CLOCK_1 / d["dt"]
 
             if self._regulator:
                 self._regulator.regulate(d)
 
-            self._logtofile("\t".join((str(i) for i in [
-                d["t"], 1.0E-06 * d["flop/s"], 1.0E-06 * d["memory-bandwidth"],
-                d["operational-intensity"], d["pkg-power_0"], d["pkg-power_1"],
-                d["dram-power_0"], d["dram-power_1"],
-                1.E-06 * d["uncore-freq_0"], 1.E-06 * d["uncore-freq_0"]
-            ])))
+            self._logtofile("\t".join(
+                str(i) for i in [
+                    d["t"], 1.0E-06 * d["flop/s"], 1.0E-06 *
+                    d["memory-bandwidth"], d["operational-intensity"],
+                    "\t".join(
+                        str(d["pkg-power_" + str(s)]) + "\t" +
+                        str(d["dram-power_" + str(s)]) + "\t" +
+                        str(1.E-06 * d["uncore-freq_" + str(s)])
+                        for s in self.sockets)
+                ]))
